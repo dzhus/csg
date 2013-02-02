640b4f47aa0cef21607c080e08e18b7ae650a6a2
Dmitry Dzhus <dima@dzhus.org>
2013-02-02 19:00:52 +0400

    Pre-generate test rays.
    Minor fixes in benchmark

--- benchmark/Benchmark.hs
+++ benchmark/Benchmark.hs
@@ -5,24 +5,10 @@
 -}
 
 import Criterion.Main
-import Data.Vector.Storable as VS
+import Data.Vector.Storable as VS hiding ((++))
 
 import Data.Vec3 hiding (distance)
 import Data.CSG
-
-
-body1 :: Body
-body1 = intersect
-        (plane origin (fromXYZ (0, -0.5, 1)))
-        (sphere origin 2.5)
-
-
-body2 :: Body
-body2 = intersect
-        (cylinder origin (fromXYZ (1, 0, 0)) 4)
-        (intersect
-         (cylinder origin (fromXYZ (0, 1, 0)) 4)
-         (cylinder origin (fromXYZ (0, 0, 1)) 4))
 
 
 -- | Pixels in meter at unit distance.
@@ -42,12 +28,12 @@ buildCartesian yaw pitch = (u, v, w)
 
 -- | Generate initial points of rays within a square region on a plane
 -- parallel to YZ.
-generateRayStarts :: Int
+generateRayPoints :: Int
                   -- ^ Total ray count.
                   -> Double
                   -- ^ Distance of a plane from the origin.
                   -> VS.Vector SVec3
-generateRayStarts rayCount distance =
+generateRayPoints rayCount distance =
     let
         !(n, sX, sY) = buildCartesian 0 0
         !p = n .^ (-distance)
@@ -55,7 +41,7 @@ generateRayStarts rayCount distance =
         -- rays. The dimension will most closely fit the required
         -- particle count number without exceeding it
         dim :: Int
-        !dim = floor $ sqrt $ fromIntegral rayCount
+        !dim = floor $ (sqrt $ fromIntegral rayCount :: Double)
         !halfDim = dim `div` 2
         -- This differs from Raycaster module by a (fromIntegral dim)
         -- term because range for ray index values are integers (as
@@ -77,21 +63,20 @@ generateRayStarts rayCount distance =
 
 
 -- | Test how long it takes to generate a vector of initial points for
--- rays using 'generateRayStarts'.
+-- rays using 'generateRayPoints'.
 testGen :: Int -> Double -> IO ()
 testGen rayCount distance = do
-  let !v = generateRayStarts rayCount distance
+  let !_ = generateRayPoints rayCount distance
   return ()
 
 
 -- | Test raycasting performance for a body.
-test :: Int
-     -- ^ Total ray count.
-     -> Double
-     -- ^ Distance of a plane from the origin.
+test :: VS.Vector SVec3
+     -- ^ Initial points of test rays. Test rays are directed along
+     -- the Ox axis.
      -> Body
      -> IO ()
-test rayCount distance body =
+test rayPoints body =
     let
         !(n, _, _) = buildCartesian 0 0
         posToTrace pos =
@@ -100,21 +85,41 @@ test rayCount distance body =
               [] -> False
               _  -> True
     in do
-      let !v = VS.map posToTrace $ generateRayStarts rayCount distance
+      let !_ = VS.map posToTrace rayPoints
       return ()
 
 
-particles :: Int
-particles = 1000 * 1000
+rays :: Int
+rays = 1000 * 1000
+
+dist :: Double
+dist = 100
+
+
+body1 :: Body
+body1 = intersect
+        (plane origin (fromXYZ (0, -0.5, 1)))
+        (sphere origin 2.5)
+
+
+body2 :: Body
+body2 = intersect
+        (cylinder origin (fromXYZ (1, 0, 0)) 4)
+        (intersect
+         (cylinder origin (fromXYZ (0, 1, 0)) 4)
+         (cylinder origin (fromXYZ (0, 0, 1)) 4))
 
 
 main :: IO ()
 main = defaultMain
        [ bgroup "Misc"
-         [ bench "Test rays generation (10M)" $ testGen particles 10
+         [ bench ("Test rays generation (" ++ show rays ++ ")" ) $
+           testGen rays dist
          ]
        , bgroup "Raycasting"
-         [ bench "body1" $ test particles 10 body1
-         , bench "body2" $ test particles 10 body2
+         [ bench "body1" $ test rs body1
+         , bench "body2" $ test rs body2
          ]
        ]
+       where
+         !rs = generateRayPoints rays dist
