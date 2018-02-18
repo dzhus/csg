9443ac68606f4f22de56550eadbd3711966c86e2
Dmitry Dzhus <dima@dzhus.org>
2018-02-18 00:20:33 +0000

    refactor: Use Turtle.Options
    Applicative code looks much nicer than cmdargs

--- exe/raycaster.hs
+++ exe/raycaster.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 {-|
 
@@ -8,12 +8,15 @@ Standalone raycaster for CSG objects powered by Gloss.
 
 |-}
 
-import Prelude
+import Prelude hiding (FilePath)
 
+import Control.Applicative
 import qualified Data.Strict.Maybe as S
 import qualified Data.Strict.Tuple as S
 
 import GHC.Float
+import Data.String
+import Data.Monoid
 
 import Graphics.Gloss.Data.Color
 import Graphics.Gloss.Data.Display
@@ -21,7 +24,9 @@ import qualified Graphics.Gloss.Data.Point as G
 import qualified Graphics.Gloss.Interface.Pure.Game as G
 import Graphics.Gloss.Raster.Field hiding (Point)
 
-import System.Console.CmdArgs.Implicit
+import Turtle.Options
+
+import Filesystem.Path.CurrentOS
 
 import Data.CSG
 import Data.CSG.Parser
@@ -46,18 +51,33 @@ data World = World { dist :: Double
                    }
 
 
--- | Static command line options for caster.
+-- | Command line options for caster.
 data Options = Options
-    { geoFile :: FilePath
-    , width :: Int
-    , height :: Int
-    , pixels :: Int
+    { geoFile    :: FilePath
+    , width      :: Int
+    , height     :: Int
+    , pixels     :: Int
     , brightRGBA :: (Float, Float, Float, Float)
-    -- ^ Color for bright surfaces parallel to view plane (RGBA)
-    , darkRGBA :: (Float, Float, Float, Float)
-    -- ^ Color for dark surfaces perpendicular to view plane (RGBA)
+    -- ^ Color for bright surfaces parallel to view plane (RGB)
+    , darkRGBA   :: (Float, Float, Float, Float)
+    -- ^ Color for dark surfaces perpendicular to view plane (RGB)
     }
-    deriving (Data, Typeable)
+
+
+optParser :: Parser Options
+optParser = Options
+  <$> argPath "geo-file" "Geometry definition file"
+  <*> (optInt "width" 'w' "Window width" <|> pure 500)
+  <*> (optInt "height" 'h' "Window height" <|> pure 500)
+  <*> (optInt "pixels" 'p' "Pixels per ray (set to 1 for top quality)"
+       <|> pure 1)
+  <*> (optRead "bright-color" 'b'
+       (fromString $
+        "Color for bright surfaces (parallel to the view plane) " <>
+        "as (R, G, B, A) with each component between 0.0 and 1.0")
+       <|> pure (0.9, 0.9, 0.9, 1))
+  <*> (optRead "dark-color" 'd' "Color for dark surfaces"
+       <|> pure (0, 0, 0, 1))
 
 
 -- | Pixels in meter at unit distance.
@@ -195,28 +215,10 @@ casterField width height pixels solid bright' dark' =
 -- | Read solid def and program arguments, run the actual caster on
 -- success.
 main :: IO ()
-main =
-    let
-        sample = Options
-                 { geoFile = def &= argPos 0 &= typ "GEO-FILE"
-                 , width = 500 &= help "Window width"
-                 , height = 500 &= help "Window height"
-                 , pixels = 1
-                   &= help ("Number of pixels to draw per point," ++
-                            "in each dimension")
-                 , brightRGBA = (0.9, 0.9, 0.9, 1)
-                   &= help ("Color for surface parallel to view plane," ++
-                            " with each component within [0..1]")
-                   &= typ "R,G,B,A"
-                 , darkRGBA = (0, 0, 0, 1)
-                   &= help "Color for surface perpendicular to view plane"
-                   &= typ "R,G,B,A"
-                 }
-                 &= program programName
-    in do
-      Options{..} <- cmdArgs sample
-      solid <- parseGeometryFile geoFile
-      case solid of
+main = do
+    Options{..} <- options (fromString programName) optParser
+    solid <- parseGeometryFile (encodeString geoFile)
+    case solid of
         Right b -> casterField width height pixels b
                    (uncurry4 makeColor brightRGBA)
                    (uncurry4 makeColor darkRGBA)
